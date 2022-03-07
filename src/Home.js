2ded9f2bbadecd90edc0663ffaad2dacdeaf120c
Ankita <ankve21@gmail.com>
2022-03-07 17:59:57 +0530

    Blog website version 1

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BlogList } from './BlogList'
+import useFetch from "./useFetch";
+
+export const Home = () => {
+
+    const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+    return (
+      <div className="home">
+        { error && <div>{ error }</div> }
+        { isPending && <div>Loading...</div> }
+        { blogs && <BlogList blogs={blogs} /> }
+      </div>
+    );
+}
